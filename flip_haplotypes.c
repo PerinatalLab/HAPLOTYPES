4169bd93be67f0793ae0130525a9ea3bea9fb2ed
julius <j.juodakis@gmail.com>
2017-11-28 17:41:06 +0100

    switched logs to go to file and out to sdout

--- flip_haplotypes.c
+++ flip_haplotypes.c
@@ -7,39 +7,34 @@
 
 using namespace std;
 
+// USAGE: zcat onechrom.vcf.gz | ./flip_haplotypes | gzip
+
 // replace \n with \t to fix reading of last field:
 void fixEndings(char *s){
 	s[strlen(s)-1] = '\t';
 }
 // check if field is non-empty
-bool checkField(char *f){
+void checkField(char *f){
 	if(f[0]=='\0'){
 		printf("ERROR: family file appears malformed\n");
-		return(true);
+		exit(1);
 	}
-	return(false);
 }
 
 int main() {
 	// INIT
-	FILE *ifp, *ffp, *ofp;
-	char inFile[] = "in.txt";
-	char outFile[] = "out.txt";
+	FILE *lfp, *ffp;
+	char logFile[] = "log.txt";
 	char famFile[] = "parents.txt";
 
-	ifp = fopen(inFile, "r");
+	lfp = fopen(logFile, "w");
 	ffp = fopen(famFile, "r");
-	ofp = fopen(outFile, "w");
-	if(ifp == NULL){
-		fprintf(stderr, "Can't open input file!\n");
+	if(lfp == NULL){
+		printf("ERROR: Can't open log file!\n");
 		exit(1);
 	}
 	if(ffp == NULL){
-		fprintf(stderr, "Can't open family info file!\n");
-		exit(1);
-	}
-	if(ofp == NULL){
-		fprintf(stderr, "Can't open output file!\n");
+		printf("ERROR: Can't open family info file!\n");
 		exit(1);
 	}
 
@@ -62,15 +57,15 @@ int main() {
 		fixEndings(linecp);
 		
 		fieldf = strsep(&linecp, "\t");
-		if(checkField(fieldf)) return(1);
+		checkField(fieldf);
 		dads.push_back(fieldf);
 
 		fieldf = strsep(&linecp, "\t");
-		if(checkField(fieldf)) return(1);
+		checkField(fieldf);
 		moms.push_back(fieldf);
 
 		fieldf = strsep(&linecp, "\t");
-		if(checkField(fieldf)) return(1);
+		checkField(fieldf);
 		fets.push_back(fieldf);
 	}
 
@@ -78,7 +73,7 @@ int main() {
 	int ntoread = moms.size();
 	char *momsc[ntoread], *dadsc[ntoread], *fetsc[ntoread];
 
-	printf("Family info file read. The following trios were found:\n");
+	fprintf(lfp, "Family info file read. The following trios were found:\n");
 	for(int i=0; i<ntoread; i++){
 		momsc[i] = new char[moms[i].size() + 1];
 		dadsc[i] = new char[dads[i].size() + 1];
@@ -87,9 +82,9 @@ int main() {
 		strcpy(dadsc[i], dads[i].c_str());
 		strcpy(fetsc[i], fets[i].c_str());
 
-		printf("%s and %s are parents of %s \n", momsc[i], dadsc[i], fetsc[i]);
+		fprintf(lfp, "%s and %s are parents of %s \n", momsc[i], dadsc[i], fetsc[i]);
 	}
-	printf("Total amount of trios found: %d\n", ntoread);
+	fprintf(lfp, "Total amount of trios found: %d\n", ntoread);
 
 	// positions of good parents in VCF
 	// (init to -1 for error catching)
@@ -101,8 +96,8 @@ int main() {
 	}
 
 	// read VCF:
-	printf("Reading VCF file\n");
-	while( (nread = getline(&line, &len, ifp)) > 0 ){
+	fprintf(lfp, "Reading VCF file\n");
+	while( (nread = getline(&line, &len, stdin)) > 0 ){
 		int fieldn = 1;
 
 		// drop info lines
@@ -126,15 +121,15 @@ int main() {
 				for(int i=0; i<ntoread; i++){
 					if(strcmp(momsc[i], field)==0){
 						momspos[i] = fieldn-10;
-						printf("found sample %s in good moms\n", field);
+						fprintf(lfp, "found sample %s in good moms\n", field);
 						break;
 					} else if(strcmp(dadsc[i], field)==0){
 						dadspos[i] = fieldn-10;
-						printf("found sample %s in good dads\n", field);
+						fprintf(lfp, "found sample %s in good dads\n", field);
 						break;
 					} else if(strcmp(fetsc[i], field)==0){
 						fetspos[i] = fieldn-10;
-						printf("found sample %s in good fets\n", field);
+						fprintf(lfp, "found sample %s in good fets\n", field);
 						break;
 					}
 				}
@@ -143,8 +138,8 @@ int main() {
 			}
 			// 9 initial fields + 1 empty after terminal \t
 			nsamples = fieldn-11;
-			printf("Header line read.\n");
-			printf("In total, read %d samples from VCF\n", nsamples);
+			fprintf(lfp, "Header line read.\n");
+			fprintf(lfp, "In total, read %d samples from VCF\n", nsamples);
 
 			break;
 		}
@@ -155,19 +150,19 @@ int main() {
 	}
 
 	// header of output:
-	fprintf(ofp, "#CHROM\tPOS\tID\tREF\tALT\tINFO");
+	printf("#CHROM\tPOS\tID\tREF\tALT\tINFO");
 	for(int i=0; i<ntoread; i++){
 		if(momspos[i]<0 || fetspos[i]<0 || dadspos[i]<0){
 			printf("ERROR: trio %s, %s, %s was not found in VCF.\n", momsc[i], dadsc[i], fetsc[i]);
-			return(1);
+			exit(1);
 		} else {
-			fprintf(ofp, "\t%s\t%s\t%s", momsc[i], dadsc[i], fetsc[i]);
+			printf("\t%s\t%s\t%s", momsc[i], dadsc[i], fetsc[i]);
 		}
 		delete dadsc[i];
 		delete momsc[i];
 		delete fetsc[i];
 	}
-	fprintf(ofp, "\n");
+	printf("\n");
 
 	// prep for reading genotypes
 	int hapA[nsamples], hapB[nsamples];
@@ -176,7 +171,7 @@ int main() {
 	int merrs = 0;
 
 	// continue to actual genotypes
-	while( (nread = getline(&line, &len, ifp)) > 0 ){
+	while( (nread = getline(&line, &len, stdin)) > 0 ){
 		int fieldn = 1;
 
 		fixEndings(line);
@@ -191,9 +186,9 @@ int main() {
 				hapB[fieldn-10] = field[2]-48;
 			} else if(fieldn==1){
 				// print out info fields:
-				fprintf(ofp, "%s", field);
+				printf("%s", field);
 			} else if(fieldn < 6 || fieldn==8){
-				fprintf(ofp, "\t%s", field);
+				printf("\t%s", field);
 			}
 			
 			fieldn++;
@@ -209,26 +204,26 @@ int main() {
 
 			if(untransmM<0 || untransmM>1 || untransmP<0 || untransmP>1){
 				// Mendelian error for the entire trio
-				fprintf(ofp, "\t.|.");
-				fprintf(ofp, "\t.|.");
-				fprintf(ofp, "\t.|.");
+				printf("\t.|.");
+				printf("\t.|.");
+				printf("\t.|.");
 				merrs++;
 			} else {
 				// print: matT|matU  patT|patU  fetM|fetP
-				fprintf(ofp, "\t%d|%d", transmM, untransmM);
-				fprintf(ofp, "\t%d|%d", transmP, untransmP);
-				fprintf(ofp, "\t%d|%d", transmM, transmP);
+				printf("\t%d|%d", transmM, untransmM);
+				printf("\t%d|%d", transmP, untransmP);
+				printf("\t%d|%d", transmM, transmP);
 			}
 		}
-		fprintf(ofp, "\n");
+		printf("\n");
 	}
-	printf("Scan complete. Total genotypes read: %d\n", nlines);
-	printf("In total, %d Mendelian errors were observed.\n", merrs);
+	fprintf(lfp, "Scan complete. Total genotypes read: %d\n", nlines);
+	fprintf(lfp, "In total, %d Mendelian errors were observed.\n", merrs);
 
 	free(line);
 	free(linef);
-	fclose(ifp);
-	fclose(ofp);
+	fclose(lfp);
+	fclose(ffp);
 
 	return(0);
 }
